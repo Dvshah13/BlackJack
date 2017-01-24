68279bb6c4f8f40327a2c458ec0dbd6fe96d05c2
Dvshah13 <dvshah13@gmail.com>
2017-01-24 16:44:17 -0600

    refactored with hidden card

--- javascript/blackjack_closure.js
+++ javascript/blackjack_closure.js
@@ -1,241 +1,241 @@
-function Card(point,suit) {
-    var point = point;
-    var suit = suit;
-    function getImageURL() {
-        var str = point;
-        switch(point) {
+function Card(point, suit) {
+
+    var getImageUrl = function() {
+        var cardValue = "";
+        switch (point) {
+            case 1:
+                cardValue = "ace";
+                break;
             case 11:
-                str = 'jack';
+                cardValue = "jack";
                 break;
             case 12:
-                str = 'queen';
+                cardValue = "queen";
                 break;
             case 13:
-                str = 'king';
+                cardValue = "king";
                 break;
-            case 1:
-                str = 'ace';
+            default:
+                cardValue = point;
         }
-        return 'images/'+ str + '_of_' + suit + '.png';
+        return "images/" + cardValue + "_of_" + suit + ".png";
+    }
+
+    var getValue = function() {
+        if (point > 10) {
+            return 10;
+        } else if (isAce()) {
+            return 11;
+        } else {
+            return point;
+        }
+    }
+
+    var isAce = function() {
+        return point == 1;
+    }
+
+    return {
+        "getImageUrl": getImageUrl,
+        "getValue": getValue,
+        "isAce": isAce
     };
-    return { "getImageURL":getImageURL() };
-};
-
-function Hand() {
-    this.cards = [];
-    this.numAces = 0;
-};
-
-Hand.prototype.addCard = function(card){
-    this.cards.push(card);
-    if(card.point == 1) {
-    this.numAces++;
 }
-};
-
-Hand.prototype.getPoints = function(card){
-    var sum=0;
-    var numAces = this.numAces;
-    this.cards.forEach(function(card){
-        if(card.point>10){
-            sum+=10;
-        }
-        else if (card.point == 1) {
-            sum += 11;
-}
-        else {
-            sum += card.point;
-        }
-        while (sum > 21 && numAces > 0) {
-            sum -= 10;
-        }
-    });
-    return sum;
-};
 
 function Deck() {
-  this.deck = [];
-  this.played = [];
-  var suits = ['spades', 'diamonds', 'clubs', 'hearts']
-  for (var j=0; j <= 3; j++){
-      for (var i=1; i<=13; i++){
-  this.deck.push(new Card(i, suits[j]));
-}};
-
-Deck.prototype.draw = function(hand) {
-    var card = this.deck.pop();
-    hand.addCard(card);
-  }
-
-Deck.prototype.shuffle = function() {
-    var currentIndex = this.deck.length;
-    while (currentIndex !== 0) {
-    var randomIndex = Math.floor(Math.random() * this.deck.length);
-    currentIndex -= 1;
-    var tempValue = this.deck[currentIndex];
-        this.deck[currentIndex] = this.deck[randomIndex];
-        this.deck[randomIndex] = tempValue;
+    var cards = [];
+    var suits = ["spades", "hearts", "clubs", "diamonds"];
+    for (i = 1; i <= 13; i++) {
+        for (s = 0; s < 4; s++) {
+            card = Card(i, suits[s]);
+            cards.push(card);
+        }
     }
-   };
+    var shuffle = function() {
+        var currentIndex = cards.length,
+            temporaryValue, randomIndex;
 
-Deck.prototype.numCardsLeft = function() {
-    return this.deck.length;
-}};
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-var playerHand = new Hand();
-var dealerHand = new Hand();
-var hiddenHand = new Hand();
-myDeck = new Deck();
-var playerWin = 0;
-var dealerWin = 0;
-var draw = 0;
-var money = 500;
-var currentBet = 0;
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-function clearBoard() {
-    $('#dealer-hand').empty();
-    $('#player-hand').empty();
-    playerHand.cards=[];
-    dealerHand.cards=[];
-    hiddenHand.cards=[];
-    $('#player-points').empty();
-    $('#dealer-points').empty();
+            // And swap it with the current element.
+            temporaryValue = cards[currentIndex];
+            cards[currentIndex] = cards[randomIndex];
+            cards[randomIndex] = temporaryValue;
+        }
+    }
+    shuffle();
+    var getCard = function() {
+        return cards.pop();
+    }
+    return {
+        getCard: getCard
+    }
 }
 
-function gameOver() {
-    document.getElementById('hit-button').style.visibility='hidden';
-    document.getElementById('stand-button').style.visibility='hidden';
-    document.getElementById('bet-button').style.visibility='hidden';
+function Hand(player) {
+    var cards = [];
+    var numAces = 0;
+
+    var addCard = function(card) {
+        if (card.isAce()) {
+            numAces++;
+        }
+        cards.push(card);
+        $("#" + player + "-hand").append("<img src='" + card.getImageUrl() + "' />");
+        $("#" + player + "-points").text(getScore());
+    }
+    var addHidden = function(card)
+    {
+        if (card.isAce()) {
+            numAces++;
+        }
+        cards.push(card);
+        var hide = true;
+        if (hide == true) {
+        $("#" + player + "-hand").append('<img src="images/back.jpg" />');
+    }
+    else {
+        $("#" + player + "-hand").append("<img src='" + card.getImageUrl() + "' />");
+    }
+    return hide;
+        // $("#" + player + "-points").text(getScore());
+}
+    var getScore = function() {
+        var score = 0;
+        for (var i = 0; i < cards.length; i++) {
+            var card = cards[i];
+            score += card.getValue();
+        }
+        //if I'm over 21 and i have aces:
+        aceIndex = 1;
+        while (aceIndex <= numAces) {
+            if (score > 21) {
+                score = score - 10;
+            }
+            aceIndex++;
+        }
+        return score;
+    }
+    return {
+        "addCard": addCard,
+        "getScore": getScore,
+        "addHidden": addHidden
+    };
 }
 
-function startGame() {
-    document.getElementById('hit-button').style.visibility='visible';
-    document.getElementById('stand-button').style.visibility='visible';
-    document.getElementById('bet-button').style.visibility='visible';
+function Game() {
+    var deck = Deck();
+    var dealerHand = Hand("dealer");
+    var playerHand = Hand("player");
+    var message = "";
+    $("#dealer-hand").empty();
+    $("#player-hand").empty();
+    $("#dealer-points").text("");
+    $("#player-points").text("");
+    $("#messages").text("");
+    var deal = function() {
+        document.getElementById('bet1').style.visibility='hidden';
+        document.getElementById('bet5').style.visibility='hidden';
+        document.getElementById('bet10').style.visibility='hidden';
+        document.getElementById('bet25').style.visibility='hidden';
+        document.getElementById('betAll').style.visibility='hidden';
+        playerHand.addCard(deck.getCard());
+        dealerHand.addCard(deck.getCard());
+        playerHand.addCard(deck.getCard());
+        if (playerHand.getScore() == 21) {
+            message = "Player Wins!";
+            endGame();
+        }
+        dealerHand.addHidden(deck.getCard());
+        if (dealerHand.getScore() == 21) {
+            message = "Dealer Wins!";
+            endGame();
+        }
+        $("#deal-button").prop("disabled", true);
+        $("#hit-button").prop("disabled", false);
+        $("#stand-button").prop("disabled", false);
+        $("#bet-button").prop("disabled", false);
+    }
+    var playerHit = function() {
+        $("#bet-button").prop("disabled", true);
+        playerHand.addCard(deck.getCard());
+        if (playerHand.getScore() > 21) {
+            message = "Player Busts!";
+            endGame();
+        }
+    }
+    var playerBet = function() {
+        document.getElementById('bet1').style.visibility='visible';
+        document.getElementById('bet5').style.visibility='visible';
+        document.getElementById('bet10').style.visibility='visible';
+        document.getElementById('bet25').style.visibility='visible';
+        document.getElementById('betAll').style.visibility='visible';
+    }
+    var playerStand = function() {
+        
+        $("#bet-button").prop("disabled", true);
+        $("#hit-button").prop("disabled", true);
+        $("#stand-button").prop("disabled", true);
+        while (dealerHand.getScore() <= 16) {
+            dealerHand.addCard(deck.getCard());
+        }
+        if (dealerHand.getScore() > 21) {
+            message = "Dealer busts";
+        } else if (dealerHand.getScore() > playerHand.getScore()) {
+            message = "Dealer wins";
+        } else if (dealerHand.getScore() == playerHand.getScore()) {
+            message = "Push";
+        } else {
+            message = "Player wins!"
+        }
+        endGame();
+    }
+    var endGame = function() {
+        $("#messages").append("<h1>" + message + "</h1>");
+        $("#hit-button").prop("disabled", true);
+        $("#stand-button").prop("disabled", true);
+        $("#bet-button").prop("disabled", true);
+        $("#deal-button").prop("disabled", false);
+        document.getElementById('bet1').style.visibility='hidden';
+        document.getElementById('bet5').style.visibility='hidden';
+        document.getElementById('bet10').style.visibility='hidden';
+        document.getElementById('bet25').style.visibility='hidden';
+        document.getElementById('betAll').style.visibility='hidden';
+    }
+    return {
+        deal: deal,
+        playerHit: playerHit,
+        playerStand: playerStand,
+        playerBet: playerBet
+    };
+}
+
+$(document).ready(function() {
+    var game;
+    $("#hit-button").prop("disabled", true);
+    $("#stand-button").prop("disabled", true);
+    $("#bet-button").prop("disabled", true);
     document.getElementById('bet1').style.visibility='hidden';
     document.getElementById('bet5').style.visibility='hidden';
     document.getElementById('bet10').style.visibility='hidden';
     document.getElementById('bet25').style.visibility='hidden';
     document.getElementById('betAll').style.visibility='hidden';
-    //clear the board
-    clearBoard();
-    //count the deck
-if (myDeck.numCardsLeft !== 52) {
-    myDeck = new Deck();
-}
-    //shuffle cards
-    myDeck.shuffle();
-    //deal and initiazlize players
-    myDeck.draw(playerHand);
-    myDeck.draw(playerHand);
-    myDeck.draw(dealerHand);
-    myDeck.draw(hiddenHand);
-
-dealerHand.cards.forEach(function(card){
-    $('#dealer-hand').append('<img src="'+card.getImageUrl+'" />');
-});
-
-playerHand.cards.forEach(function(card){
-    $('#player-hand').append('<img src="'+card.getImageUrl+'" />');
-});
-hiddenHand.cards.forEach(function(card){
-    $('#dealer-hand').append('<img src="images/back.jpg" />');
-});
-
-dealerPoints = dealerHand.getPoints();
-playerPoints = playerHand.getPoints();
-
-$('#player-points').append(playerPoints);
-$('#dealer-points').append(dealerPoints);
-if (playerPoints == 21 && dealerPoints !== 21) {
-    $('#player-points').html('<h3>"BLACKJACK, You WIN!"</h3>');
-    gameOver();
-}
-else if (dealerPoints == 21) {
-    $('#player-points').html('<h3>"Dealer had BLACKJACK, You LOSE!"</h3>');
-    gameOver();
-}
-}
-
-function hit() {
-    $('#player-hand').empty();
-    $('#player-points').empty();
-    $('#dealer-points').empty();
-    myDeck.draw(playerHand);
-    playerHand.cards.forEach(function(card){
-        $('#player-hand').append('<img src="'+card.getImageUrl+'" />');
-});
-
-dealerPoints = dealerHand.getPoints();
-playerPoints = playerHand.getPoints();
-$('#player-points').append(playerPoints);
-$('#dealer-points').append(dealerPoints);
-if (playerPoints > 21) {
-    $('#player-points').html('<h3>"BUST, You LOSE!"</h3>');
-    gameOver();
-
-
-    // alert("Please Play Again");
-}
-}
-
-function stand() {
-    $('#dealer-hand').empty();
-    $('#player-points').empty();
-    $('#dealer-points').empty();
-
-    dealerPoints = dealerHand.getPoints();
-    playerPoints = playerHand.getPoints();
-    $('#player-points').append(playerPoints);
-    $('#dealer-points').append(dealerPoints);
-
-    while (dealerPoints < 17) {
-        $('#dealer-hand').empty();
-        myDeck.draw(dealerHand);
-        dealerHand.cards.forEach(function(card){
-            $('#dealer-hand').append('<img src="'+card.getImageUrl()+'" />');
-            $('#dealer-points').empty();
-        dealerPoints = dealerHand.getPoints();
-        $('#dealer-points').append(dealerPoints);
-    })
-}
-    if (dealerPoints > 21 || dealerPoints < playerPoints) {
-        $('#player-points').html('<h3>"Yay, You WIN!"</h3>');
-        gameOver();
-    }
-    else if(dealerPoints > playerPoints) {
-        $('#player-points').html('<h3>"Sorry, You Lose, Dealer Wins!"</h3>');
-        gameOver();
-    }
-    else {
-        $('#player-points').html('<h3>"You Draw!"</h3>');
-        gameOver();
-    }
-}
-
-function bet() {
-    document.getElementById('bet1').style.visibility='visible';
-    document.getElementById('bet5').style.visibility='visible';
-    document.getElementById('bet10').style.visibility='visible';
-    document.getElementById('bet25').style.visibility='visible';
-    document.getElementById('betAll').style.visibility='visible';
-}
-
-
-$("document").ready(function() {
-
-$("#deal-button").click(function() {
-    startGame();
-});
-
-$("#hit-button").click(function() {
-    hit();
-});
-
-$("#stand-button").click(function() {
-    stand();
-});
-$("#bet-button").click(function() {
-    bet();
-});
+    $("#deal-button").click(function() {
+        game = Game();
+        game.deal();
+    });
+    $("#hit-button").click(function() {
+        game.playerHit();
+    });
+    $("#stand-button").click(function() {
+        game.playerStand();
+    });
+    $("#bet-button").click(function() {
+        game.playerBet();
+    });
 });
